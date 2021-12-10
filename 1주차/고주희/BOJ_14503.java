51d8e56b0ae939d3cdbc3efd9d2075be30475853
ko509 <62974220+ko509@users.noreply.github.com>
2021-12-10 23:48:24 +0900

    로봇 청소기

--- 1주차/고주희/BOJ_14503.java
+++ 1주차/고주희/BOJ_14503.java
@@ -15,7 +15,7 @@ public class BOJ_14503 {
 	static int[] dr = {-1,0,1,0};
 	static int[] dc = {0,1,0,-1};
 	public static void main(String[] args) throws IOException {
-
+		// 입력받기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -35,11 +35,15 @@ public class BOJ_14503 {
 				room[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		// 로봇청소기 초기 위치 청소 => count도 증가
 		room[rr][rc] = 2;
 		int count = 1;
-		int fail = 0;
+		// 탐색 실패 횟수
+		int fail = 0; 
 		while(true) {
+			// 왼쪽을 청소할 수 있는가?
 			if(cangoleft(rr, rc, rd, room)) {
+				// 있으면 방향, 위치 바꾸고 해당 자리 청소, 지금까지 모았던 fail값 0으로
 				rd = (rd+3)%4;
 				rr +=dr[rd];
 				rc +=dc[rd];
@@ -51,30 +55,40 @@ public class BOJ_14503 {
 //				}
 //				System.out.println();
 			}else {
+				// 청소할 수 없으면 fail값++
 				fail++;
+				// 방향 바꿔주고
 				rd = (rd+3)%4;
+				// 만일 사방이 다 청소할 수 없다면
 				if(fail==4) {
+					// 후진할 수 있는 지 확인
 					if(canback(rr, rc, rd, room)) {
+						// 후진 가능한 경우 위치 변경, 방향은 변경하지 않음
 						int nd = (rd+2)%4;
 						rr +=dr[nd];
 						rc +=dc[nd];
 					}else {
+						// 후진 불가능한 경우 while문 break
 						break;
 					}
+					//후진했다면 fail값 초기화
 					fail = 0;
 				}
 				
 			}
 		}
+		// count 출력
 		System.out.println(count);
 		
 	}
+	// 후진 가능한지 알아내는 함수
 	private static boolean canback(int rr, int rc, int rd, int[][] room) {
 		int nd = (rd+2)%4;
 		if(room[rr+dr[nd]][rc+dc[nd]]==1) return false;
 		else return true;
 		
 	}
+	// 왼쪽 칸이 청소 가능한 칸인지 알아보는 함수
 	private static boolean cangoleft(int rr, int rc, int rd, int[][] room) {
 		int nd = (rd+3)%4;
 		if(room[rr+dr[nd]][rc+dc[nd]]==0) return true;
