d707309f8dc064fd79eaaf8734086540a1ed8d43
ko509 <62974220+ko509@users.noreply.github.com>
2022-01-07 15:32:42 +0900

    서로소 집합

---
+++ 4주차/고주희/SWEA_3289.java
@@ -0,0 +1,66 @@
+package week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_3289 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			StringBuilder sb = new StringBuilder();
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int[] parent = new int[N];
+			int[] rank = new int[N];
+			for (int i = 0; i < N; i++) {
+				parent[i] = i;
+			}
+			
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int op = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken())-1;
+				int b = Integer.parseInt(st.nextToken())-1;
+				
+				if(op==0) {
+					union(a,b,parent, rank);
+				}else {
+					int xa = find(a, parent);
+					int xb = find(b, parent);
+					sb.append(xa==xb? 1:0);
+				}
+			}
+			System.out.println("#"+t+" "+sb.toString());
+		}
+	}
+
+	private static void union(int a, int b, int[] parent, int[] rank) {
+
+		int xa = find(a, parent);
+		int xb = find(b, parent);
+		if(xa==xb) return;
+		if(rank[xa]>=rank[xb]) {
+			parent[xb] = xa;
+			rank[xa]++;
+		}else {
+			parent[xa] = xb;
+			rank[xb]++;
+		}
+	}
+
+	private static int find(int a, int[] parent) {
+		if(parent[a]==a) return a;
+		else return parent[a] = find(parent[a], parent);
+	}
+
+}
