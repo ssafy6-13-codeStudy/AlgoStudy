f9efd9ca49a661de1963946b85e4a76a04e4318b
jay-one11 <73393475+jay-one11@users.noreply.github.com>
2022-02-03 20:18:19 +0900

    Create Main_1013_Contact.java

---
+++ 7주차/Main_1013_Contact.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+
+// (100+ 1+ | 01)+
+// 1. 01로 시작하는지 100 *N 1 *M 으로 시작하는지
+// 2. 완성된 패턴이 반복되는지
+
+public class Main_1013_Contact {
+	static int T;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException{
+//		T = Integer.parseInt(br.readLine());
+		int[] a = {1,2,3,4,5,6};
+		System.out.println(Arrays.toString(a));
+		String pattern = "(100+1+|01)+";
+		for(int t=0; t<T; t++) {
+			boolean flag;
+			String s = "";
+			
+			
+//			int idx =0;
+//			String cur = br.readLine();
+//			if(cur.matches(pattern)) System.out.println("YES");
+//			else System.out.println("NO");
+		}
+	}
+}
